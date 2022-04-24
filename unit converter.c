57243c9ec3b8fc696e6edfb7062571dd9abea310
Kundanikapradhankl <103298138+Kundanikapradhankl@users.noreply.github.com>
2022-04-24 09:11:39 +0530

    Update unit converter.c

--- unit converter.c
+++ unit converter.c
@@ -5,20 +5,20 @@ int main() {
   int tempChoice;
   int currencyChoice;
   int massChoice;
-  int userinputF; // User inputted Fahreinheit;
-  int userinputC; // User inputted Celsius;
+  int F; // User inputted Fahreinheit;
+  int C; // User inputted Celsius;
   int userinputUSDtoEuro; // User inputted for USD to EURO;
   int userinputUSDtoJPY; // User inputted for USD to JPY;
   int userinputUSDtoRMB; // User inputted for USD to RMB;
-  int userinputOunce; // User inputted for Ounce;
-  int userinputGram; // User inputted for Gram;
+  int Ounce; // User inputted for Ounce;
+  int Gram; // User inputted for Gram;
   int fahrenheitToCelcius; // variable that stores the converted F->C;
   int celciusToFahrenheit; // variable that stores the converted C->F;
   float USDtoEURO ; // varaible that stores the converted USD->EURO;
   float USDtoJPY; // stores the converted USD->JPY;
   float USDtoRMB; // stores the converted USD->RMB;
   float ounceToPounds; // stores the converted Ounce->Pounds;
-  float gramsToPounds; // stores the vonerted Grams->Pounds;
+  float gramsToPounds; // stores the converted Grams->Pounds;
   
   printf("Welcome to Unit Converter! \n");
   printf("Here is a list of conversation to choose from: \n");
@@ -34,14 +34,14 @@ int main() {
       scanf("%d",&tempChoice);
       if(tempChoice == 1){
           printf("Please enter the Fahrenheit degree: \n");
-          scanf("%d",&userinputF);
-          fahrenheitToCelcius =  ((userinputF-32) * (5.0/9.0));
+          scanf("%d",&F);
+          fahrenheitToCelcius =  ((F-32) * (5.0/9.0));
           printf("Celcius: %d",fahrenheitToCelcius);
       }
       else if(tempChoice == 2){
         printf("Please enter the Celcius degree: \n");
-        scanf("%d",&userinputC);
-        celciusToFahrenheit = ((9.0/5.0)*userinputC + 32);
+        scanf("%d",&C);
+        celciusToFahrenheit = ((9.0/5.0)*C + 32);
         printf("Fahrenheit: %d",celciusToFahrenheit);
       }
       else
@@ -84,14 +84,14 @@ int main() {
       scanf("%d",&massChoice);
       if(massChoice == 1){
           printf("Please enter the ounce amount: \n");
-          scanf("%d",&userinputOunce);
-          ounceToPounds = userinputOunce * 0.0625;
+          scanf("%d",&Ounce);
+          ounceToPounds = Ounce * 0.0625;
           printf("Pounds: %.2f",ounceToPounds);
       }
       else if(massChoice == 2) {
           printf("Please enter the gram amount: \n");
-          scanf("%d",&userinputGram);
-          gramsToPounds = userinputGram * 0.00220462;
+          scanf("%d",&Gram);
+          gramsToPounds = Gram * 0.00220462;
           printf("Pounds: %.2f",gramsToPounds);
       }
       else 
